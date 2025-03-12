0065fdf2161ba5a7075d786caaca8ba6492b9391
Sameer Rawat <b22172@students.iitmandi.ac.in>
2025-03-12 19:16:38 +0530

    Add snake step logic

--- SnakeGame.py
+++ SnakeGame.py
@@ -13,10 +13,43 @@ class SnakeGame:
     
     def reset(self):
         self.direction = 'RIGHT'
-        self.snake = [[WIDTH // 2, HEIGHT // 2]]    # start at the center
+        self.head = [WIDTH // 2, HEIGHT // 2]
+        self.snake = [self.head, [self.head[0] - BLOCK_SIZE, self.head[1]], [self.head[0] - (2 * BLOCK_SIZE), self.head[1]]]
+        self.score = 0
         self.food = self.create_food()
+        self.iteration = 0
     
+    def play_step(self, action):
+        self.iteration += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        
+        reward = 0
+        game_over = False
+
+        self.move(action)   
+        self.snake.insert(0, self.head)
+
+        if self.food in self.snake:
+            reward = 100
+            self.score += 1
+            self.food = self.create_food()
+        else:
+            self.snake.pop()
+        
+        if self.collision():
+            reward = -100
+            game_over = True
+        
+        return reward, game_over, self.score
+
     def create_food(self):
         return [random.randrange(0, WIDTH, BLOCK_SIZE), random.randrange(0, HEIGHT, BLOCK_SIZE)]
+    
+    def move(self, action):
+        pass
 
-    # TODO: Step (L, R, U, D), collision detection (wall, self, food), game over
+    def collision(self):
+        pass
